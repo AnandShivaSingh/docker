4a0a2e2f103f4cf08b0699e148db9cb1fe76b6ee
root <root@dock.c.bright-meridian-214404.internal>
2019-02-22 12:52:34 +0000

    s

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Anand Shiva singh');
+  res.send('Anand singh');
 });
-app.listen(8000, () => {
-  console.log('Listening on port 8000');
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
 });
